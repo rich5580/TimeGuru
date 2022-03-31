3da469c3722103ff4feaa4fb8be4020ed258c857
rich5580 <rich5580@mylaurier.ca>
2022-03-31 14:46:45 -0400

    Initial Commit

---
+++ TimeGuru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeGuru
+//
+//  Created by Harlan Rich on 2022-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
