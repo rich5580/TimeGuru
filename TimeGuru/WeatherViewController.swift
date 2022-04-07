b87405feaee81a0def0d5ba7976816a511bd086b
MereCube905 <samuelhst@gmail.com>
2022-04-07 16:12:54 -0400

    Weather Code
    Weather page backend is nearly complete

--- TimeGuru/WeatherViewController.swift
+++ TimeGuru/WeatherViewController.swift
@@ -11,52 +11,17 @@ let apiKey = "8741296de0b3a921b78384ba552a3995"
 
 var JSONData: Data?
 
-struct WeatherStr: Codable{
-    var main: String?
-    var description: String?
+private struct WeatherResult: Codable{
+    var weather: [weather]
+    var main: main
 }
-struct WeatherNum: Codable{
-    var temp: Double?
+private struct main: Codable{
+    var temp: Double
 }
-struct WeatherMain: Codable{
-    let main: WeatherNum
+private struct weather: Codable{
+    var main: String
+    var description: String
 }
-struct WeatherW: Codable{
-    let weather: WeatherStr
-}
-
-func decodeJSONData(JSONData: Data){
-    do{
-        let weatherNumData = try? JSONDecoder().decode(WeatherMain.self, from: JSONData)
-        let weatherStrData = try? JSONDecoder().decode(WeatherW.self, from: JSONData)
-        if let weatherNumData = weatherNumData, let weatherStrData = weatherStrData{
-            let weatherN = weatherNumData.main
-            let weatherS = weatherStrData.weather
-            print(weatherN.temp!)
-            print(weatherS.main!)
-            print(weatherS.description!)
-        }
-    }
-}
-
-func loadJSONData(url: URL?){
-    let task = URLSession.shared.dataTask(with: url!) { data, response, error in
-        if let error = error{
-            print("Error : \(error.localizedDescription)")
-        }
-        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-            print("Error : HTTP Response Code Error")
-            return
-        }
-        guard let data = data else {
-            print("Error : No Response")
-            return
-        }
-        decodeJSONData(JSONData: data)
-    }
-    task.resume()
-}
-
 
 class WeatherViewController: UIViewController {
 
@@ -66,6 +31,49 @@ class WeatherViewController: UIViewController {
         let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=imperial")
         // Do any additional setup after loading the view.
         loadJSONData(url: url)
+    }
+    
+    func decodeJSONData(JSONData: Data){
+        do{
+            let weatherData = try? JSONDecoder().decode(WeatherResult.self, from: JSONData)
+            if let weatherData = weatherData{
+                let weatherN = weatherData.main
+                print(weatherN.temp)
+                let weatherS = weatherData.weather
+                print(weatherS[0].main)
+                print(weatherS[0].description)
+                
+                let currentDateTime = Date()
+                // get the user's calendar
+                let userCalendar = Calendar.current
+                // choose which date and time components are needed
+                let requestedComponents: Set<Calendar.Component> = [.hour]
+                // get the components
+                let dateTimeComponents = userCalendar.dateComponents(requestedComponents, from: currentDateTime)
+                // now the components are available
+                print(dateTimeComponents.hour!)
+            }
+
+        }
+    }
+
+    func loadJSONData(url: URL?){
+        
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            if let error = error{
+                print("Error : \(error.localizedDescription)")
+            }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                print("Error : HTTP Response Code Error")
+                return
+            }
+            guard let data = data else {
+                print("Error : No Response")
+                return
+            }
+            self.decodeJSONData(JSONData: data)
+        }
+        task.resume()
     }
     
 
